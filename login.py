f5bf56d4ebd33573246d100e1b95bdd2f0c71427
Teresa Newman <tnewman9219@gmail.com>
2025-04-15 15:39:21 -0400

    init

--- login.py
+++ login.py
@@ -1,1 +1,10 @@
-from tkinter import Tk
+import tkinter
+
+window = tkinter.Tk()
+window.title("login form")
+window.geometry('340x440')
+
+label = tkinter.Label(window, text = "Login")
+label.pack()
+
+window.mainloop()
