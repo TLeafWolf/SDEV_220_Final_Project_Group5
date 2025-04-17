1a8075a13be1714a6484a7efb8d49d6d2686d375
Teresa Newman <tnewman9219@gmail.com>
2025-04-17 12:56:33 -0400

    init

--- login.py
+++ login.py
@@ -1,51 +1,85 @@
 # this program opens a window display in vs code when it is ran as of right now
 
+import customtkinter
+import sqlite3
 import tkinter
-#imports message box for popups
+
 from tkinter import messagebox
 
+connection = sqlite3.connect('users.db')
 
-window = tkinter.Tk()
-window.title("login form")
-window.geometry('340x440')
-window.configure(bg = '#333333')
+cursor = connection.cursor()
 
-# the def for logging in. will obviously need to repelace the stuff inside of it as right now it is a place holder account information
-def login():
-    username = "BobMax"
-    password = "12345"
-    if username_entry.get() == username and password_entry.get() == password:
-        messagebox.showinfo(title = "Login Success", message = "You successfully logged in.")
-    else:
-        messagebox.showinfo(title = "Error", message = "Invalid login.")
+command1 = """
+    CREATE TABLE IF NOT EXISTS
+    USERS(username text, password text)
+"""
+cursor.execute(command1)
+cursor.execute("INSERT INTO USERS VALUES('user1', 'password1')")
+cursor.execute("INSERT INTO USERS VALUES('user2', 'password2')")
+cursor.execute("INSERT INTO USERS VALUES('user3', 'password3')")
+cursor.execute("INSERT INTO USERS VALUES('user4', 'password4')")
 
-# make the gui responsive to the screen. it allows it to change size and location based on the size of the screen and keeps it centered
-frame = tkinter.Frame(bg = '#333333')
 
-# creating them
-# the ones with label are the text ion it own
-# entry is the boxes to put user information
-#and the button is th ebutton
-# bg is background color
-#fg is font color
-#font is the fontsize and font type
-login_label = tkinter.Label(frame, text = "Login", bg = '#333333', fg = "#008000", font =("Arial",30))
-username_label = tkinter.Label(frame, text = "Username", bg = '#333333', fg = "#FFFFFF", font =("Arial",16))
-username_entry = tkinter.Entry(frame, font =("Arial",16))
-password_entry = tkinter.Entry(frame, show ="*", font =("Arial",16))
-password_label = tkinter.Label(frame, text = "Password", bg = '#333333', fg = "#FFFFFF", font =("Arial",16))
-login_button = tkinter.Button(frame, text = "Login", bg = '#008000', fg = "#FFFFFF", font =("Arial",16), command = login)
 
-# placing them on the screen
-# row and colums is how w eplace it left to right and up and down
-# pady is the padding on the y axis it adds space between the object on other objects in the vertical direction
-login_label.grid(row = 0, column=0, columnspan=2, sticky=  "news", pady = 40)
-username_label.grid(row = 1, column=0)
-username_entry.grid(row = 1, column=1, pady = 20)
-password_label.grid(row = 2, column=0)
-password_entry .grid(row = 2, column=1, pady = 20)
-login_button .grid(row = 3, column=0, columnspan=2, pady = 30)
+customtkinter.set_appearance_mode("light")
+customtkinter.set_default_color_theme("green")
 
-# displays the frame that is making the form center of screen
-frame.pack()
-window.mainloop()
+app = customtkinter.CTk()
+
+app.title("Inventory Management Login")
+app.geometry("960x540")
+
+bottomframe = tkinter.Frame(app)
+bottomframe.pack(expand = True)
+
+def button_event():
+    cursor.execute("SELECT * from USERS")
+    results = cursor.fetchall()
+    for result in results:
+        if user_name.get() == result[0] and password.get() == result[1]:
+            messagebox.showinfo(title = "Login Success", message = "You successfully logged in.")
+            break
+        else:
+            messagebox.showinfo(title = "Error", message = "Invalid login.")
+
+
+user_name = customtkinter.CTkEntry(master=bottomframe,
+                                   placeholder_text="Username",
+                                  font =('Helvetica', 28),
+                                   width =800,
+                                   height=100,
+                                   border_width=2,
+                                   corner_radius = 30)
+user_name.pack()
+
+password = customtkinter.CTkEntry(master=bottomframe,
+                                   placeholder_text="Password",
+                                  font =('Helvetica', 28),
+                                   width =800,
+                                   height=100,
+                                   border_width=2,
+                                   corner_radius = 30,
+                                   show = "*")
+password.pack()
+
+button = customtkinter.CTkButton(master=bottomframe,
+                                 width =500,
+                                 height=100,
+                                 border_width=0,
+                                 corner_radius = 8,
+                                 text = "Log In",
+                                 font =('Helvetica', 28),
+                                 command = button_event)
+button.pack()
+
+label = customtkinter.CTkLabel(master=bottomframe,
+                               text ="",
+                               text_color="black",
+                                width =200,
+                                 height=100,
+                                 font =('Helvetica', 28),
+                                 fg_color = ("white"),
+                                 corner_radius = 8)
+label.pack()
+app.mainloop()
