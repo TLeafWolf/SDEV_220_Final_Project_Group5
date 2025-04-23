a7096f9481fd1b46081ca45bc346a76c73bc5524
TLeafWolf <tnewman34@ivytech.edu>
2025-04-23 19:42:49 -0400

    Delete myproject directory

--- myproject/inventory_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import InventoryItem
-
-class InventoryItemForm(forms.ModelForm):
-    class Meta:
-        model = InventoryItem
-        fields = ['price', 'quantity', 'location']  # Only include fields to be updated
